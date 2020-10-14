52b5e69f3f9eb0ec8f96b49536208fa5fad6e5a0
Natanael da Silva <66763596+NatanSiilva@users.noreply.github.com>
2020-10-14 01:50:46 -0300

    Day 1

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,33 @@
+//Create map
+
+const map = L.map('mapid').setView([-29.9445521,-51.1010502], 10)
+
+
+//create and add tileLayer
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+
+//Create icon
+
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+//create popup overllay 
+
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240,
+}).setContent('Lar das meninas <a href="orfhanage.html?=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+
+//Create and add marker
+
+L
+.marker([-29.9445521,-51.1010502], { icon })
+.addTo(map)
+.bindPopup(popup)
