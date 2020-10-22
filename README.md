dc2725aa6d988689fcd2d2eac7ea13979456dfad
Natanael da Silva <66763596+NatanSiilva@users.noreply.github.com>
2020-10-22 12:25:42 -0300

    Commit Finish
    Filnalização do Projeito .

---
+++ README.md
@@ -0,0 +1,80 @@
+
+<p align="center">
+  <img   src="public/images/logo.svg">
+<h1 align="center">Next Level Week 3 - Happy</h1>
+  </p>
+
+## Sobre o projeto :book:
+
+
+O Happy é uma plataforma para localização e agendamento de visitas em lares de acolhimento de crianças. O Happy foi pensado em homenagem ao Dia das Crianças, com o intuito de trazer mais amor e carinho para crianças que vivem em situação de vulnerabilidade social.
+<br>
+O projeto foi desenvolvido durante a Next Level Week 3, organizada pela Rocketset. O evento aconteceu durante os dias 12 a 16 de Outubro de 2020. A trilha seguida foi a Discovered, ministrada pelo professor Mayk Brito.
+<br>
+
+## Preview ::computer:
+
+  :heavy_check_mark: Layout responsivo para todos os tipos de aparelhos
+  	<br>
+
+ <p align="center">
+  <img   src="demos/page.png">
+    </p>
+  
+  
+  ## Funcionalidades :gear:
+  
+  :heavy_check_mark: Visualizar os lares de adoção através do mapa da sua região
+  <br>
+   <p align="center">
+    <img   src="demos/page2.png">
+  </p>
+    <br>
+   
+   <br>
+    :heavy_check_mark: Cadastrar um novo lar
+
+   <p align="center">
+      <br>
+    <img   src="demos/page3.png">
+  </p>
+  
+ ## Tecnologias :hammer_and_wrench:
+ - CSS3
+ - HTML5
+ - JavaScript
+ - Handlebars
+ - Node.js
+ - Express
+ - SQLite
+ 
+ ## Como rodar no seu computador
+ 
+ - Clone o repositório usando o Git Bash
+ 
+ ```
+$ git clone https://github.com/gabrielipavlack/Happy.git
+
+```
+
+- Entre na pasta do projeto:
+
+ ```
+$ cd Happy
+ ```
+
+- Instale o NPM:
+ ```
+$ npm install
+ ```
+- Execute a aplicação:
+ ```
+$ npm start
+ ```
+
+- Abra o seu navegador com o seguinte link:
+ ```
+http://localhost:5500/
+ ```
+
+Desenvolvido 💜 By Natanael da Silva 
--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,61 +1,62 @@
+
 const options = {
-    draggings: false,
-    touchZoom: false,
-    doubleClickZoom: false,
-    scrollwheelZoom: false,
-    zoomControl: false
+  draggins: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false
 }
 
-//Create map
-
-const map = L.map('mapid', options).setView([-29.9445521,-51.1010502], 10)
 
 
-//create and add tileLayer
+// create map 
+const map = L.map('mapid', options).setView([-29.6871013,-53.8278435], 15);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
-
-//Create icon
-
-const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
-})
-
-
-//Create and add marker
-
-L
-.marker([-29.9445521,-51.1010502], { icon })
+// create and add tileLayer 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
 .addTo(map)
 
 
+// create icon 
+const icon = L.icon({
+    iconUrl: "/images/map-marker.svg",
+    iconSize: [58,68],
+    iconAnchor: [29,68],
+    popupAnchor: [170, 2]
+})
 
+// create and add marker
+L.marker([-29.6871013,-53.8278435], {icon}).addTo(map)
+
+
+/* image gallery */
 
 function selectImage(event){
+  const button = event.currentTarget;
 
-    const button = event.currentTarget
-   
+  // remover todas as classes .active 
+  const buttons = document.querySelectorAll(".images button")
+  buttons.forEach(removeActiveClass)
 
-    //remove todas as classes .active
+  function removeActiveClass(button){
+    button.classList.remove("active")
+  }
 
-    const buttons = document.querySelectorAll(".images button")
-    buttons.forEach((removeActiveClass))
 
-    function removeActiveClass(button) {
-        button.classList.remove("active")
-    }
+//selecionar a imagem clicada
 
-    //selecionar a image clicada
-    const image = button.children[0]
-    const imageConatiner = document.querySelector(".orphanage-details > img")
-    //selecionar o contaiener de image
+const image = button.children[0]
+const imageContainer = document.querySelector(".orphanage-details > img")
 
-    imageConatiner.src = image.src
+//atualizar o container de image
+imageContainer.src = image.src
 
-    //adcionar a classe .active para este botão
+// adicionar a classe .active para o botão clicado
+button.classList.add('active')
 
-    button.classList.add("active")
+
+
 }
+
+
+ 
